7f3274a32dda2c7b495f310274d8db2e08c87859
Jorge Luis Maureira A <jlmaureiraa@gmail.com>
2024-11-20 20:23:21 -0300

    ajuste del home

--- app/home.php
+++ app/home.php
@@ -34,7 +34,7 @@ $totalRows_qrClinicaOrigen = $qrClinicaOrigen->RecordCount();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Panel TrackGes</title>
+  <title>Panel TEST</title>
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <!-- Font Awesome (para íconos) -->
