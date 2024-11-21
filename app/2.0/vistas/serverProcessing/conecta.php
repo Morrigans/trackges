f37bcc484715041b0f290951c77d63267c4ca87e
knito09 <123778521+knito09@users.noreply.github.com>
2024-11-21 00:35:17 -0300

    Update conecta.php

--- app/2.0/vistas/serverProcessing/conecta.php
+++ app/2.0/vistas/serverProcessing/conecta.php
@@ -2,7 +2,7 @@
 // SQL server connection information
 $sql_details = array(
     'user' => 'redges',
-    'pass' => 'oBll76w6o7<S',
+    'pass' => 'oBll76w6o7S',
     'db'   => 'redgescl_cli_santiago',
     'host' => 'database-1.cxgs8m8wkp0r.us-east-1.rds.amazonaws.com' 
 );
