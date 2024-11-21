90cfb1d45f8ed281b37e6fcc3ec3e4240868faf3
knito09 <123778521+knito09@users.noreply.github.com>
2024-11-21 00:35:59 -0300

    Update conectar.php

--- app/login/conectar.php
+++ app/login/conectar.php
@@ -1,5 +1,5 @@
 <?php 
-$conn = mysqli_connect("database-1.cxgs8m8wkp0r.us-east-1.rds.amazonaws.com","redges","oBll76w6o7<S","redgescl_cli_santiago");
+$conn = mysqli_connect("database-1.cxgs8m8wkp0r.us-east-1.rds.amazonaws.com","redges","oBll76w6o7S","redgescl_cli_santiago");
  
 if(!$conn){
 	die("Connection error: " . mysqli_connect_error());	
